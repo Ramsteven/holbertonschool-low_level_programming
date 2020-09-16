322cba0d4a527ee51d7231c2618c7b50144bb263
ramsteven <ramsteven@gmail.com>
2020-09-16 22:52:31 +0000

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+  * main - print text on the terminal
+  * @void: no get nothing
+  *
+  * This function only print on screen thex inside puts()
+  * Return:  always return 0
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

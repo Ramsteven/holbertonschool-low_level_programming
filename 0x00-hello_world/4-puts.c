63a90cf7b5246ef2cb1f92e104f16786c4d8ee49
ramsteven <ramsteven@gmail.com>
2020-09-16 22:39:23 +0000

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 /**
   * main - print text on the terminal
+  * @void: no get nothing
   *
-  *
-  * return - always return 0
+  * This function only print on screen thex inside pusth()
+  * Return:  always return 0
   */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

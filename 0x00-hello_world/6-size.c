e05d58493e5acc223865420812accca99d299bd4
ramsteven <ramsteven@gmail.com>
2020-09-16 23:45:41 +0000

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+  * main - print text on the terminal
+  * @void: no get nothing
+  *
+  * This function only print on screen thex inside puts()
+  * Return:  always return 0
+  */
+
+int main(void)
+{	
+	char c;
+	int i;
+	long int li;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int : %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}

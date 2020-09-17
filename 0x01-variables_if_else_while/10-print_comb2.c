1beddc54d6d6f969a80af67a18459c17d766ab79
ramsteven <ramsteven@gmail.com>
2020-09-17 23:58:18 +0000

    task10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+  * main - print with out puts or printf
+  * @void: nothing parameter expected
+  *
+  * print the numbers 00 to 99
+  * Return: 0
+  */
+int main(void)
+{
+	int c;
+	int x;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+			for (x = '0'; x <= '9'; x++)
+			{
+				putchar(c);
+				putchar(x);
+
+				if (x != '9' ||  c != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	}
+	putchar('\n');
+	return (0);
+}

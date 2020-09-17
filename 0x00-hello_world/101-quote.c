381a093f2c30b990e271859cbc997f772f5a8a90
ramsteven <ramsteven@gmail.com>
2020-09-17 03:15:27 +0000

    task101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+/**
+  * main - function print without print or puts
+  * @void:  nothig here
+  *
+  * Print a char divide
+  * Return: 1 always
+  */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+	return (1);  
+} 

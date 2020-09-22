e06f65cb0736a174ca0d2909b84abe8d8014bad0
ramsteven <ramsteven@gmail.com>
2020-09-22 23:52:03 +0000

    adding task 5 holberton file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,20 @@ int print_sign(int n)
 {
 	int x;
 
-	if (n > 0){
+	if (n > 0)
+	{
 		_putchar('+');
-		x=1;
+		x = 1;
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		x=0;
+		x = 0;
 	}
 	else
 	{
 		_putchar('-');
-		x=-1;
+		x = -1;
 	}
 	return (x);
 }

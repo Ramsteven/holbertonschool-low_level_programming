e7a77e85f6a80e136ec61d2971d9fb0839d63b64
ramsteven <ramsteven@gmail.com>
2020-09-22 17:10:02 +0000

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+  * main - check the code for Holberton School students.
+  *
+  * Return: Always 0.
+  */
+void print_alphabet(void)
+{
+	char n;
+	
+	for(n='z'; n<='a';n--)
+		 _putchar(n);
+
+}

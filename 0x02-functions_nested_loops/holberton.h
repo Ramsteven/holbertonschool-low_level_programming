fb0ee783a039721f4ed762b72a348d3a98ab5ce9
ramsteven <ramsteven@gmail.com>
2020-09-22 22:36:34 +0000

    adding file holberton v2

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,3 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 /* _ putchar */
 int _putchar(char);
 
@@ -12,4 +9,4 @@ void print_alphabet_x10(void);
 
 /* function that checks for lowercase return 1 o 0 otherwise */
 int _islower(int c);
-#endif
+

5d3394558256f8cfb53ed1c3d6874f51638e8e6c
ramsteven <ramsteven@gmail.com>
2020-09-22 16:45:03 +0000

    adding0x02

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+  * main - call function _putchar through the file holberton.h
+  * nothing here
+  *
+  * Return: On success 0.
+  * print the chart in secuence finally "Holberton"
+ */
+int main(void)
+{
+	_putchar('c');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* _ putchar */
+int _putchar(char);
+
+#endif	

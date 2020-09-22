a62a362cbebe8497f4fd56e975c290fe60f3ff9c
ramsteven <ramsteven@gmail.com>
2020-09-22 22:56:48 +0000

    task1v2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 
 /**
-  * Print alphabeth
+  * print_alphabet- use only user defined variables to create output
+  *
+  * Return: print to stdout the alphabet, in lowercase, followed by a new line.
   */
 void print_alphabet(void)
 {

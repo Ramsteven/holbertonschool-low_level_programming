f1433d25d2db670612d4548208d4ad0cbdced784
ramsteven <ramsteven@gmail.com>
2020-09-23 01:18:25 +0000

    task3v2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,16 @@
 
 /**
   * _islower - check if c is a lowercase
+  *@c: parameter of c variable 
   *
   * Return 1 if is lower case otherwise return 0
   */
 
 int _islower(int c)
 {
-	if (islower(c) > 0)
-	{
-		return (1);
-	}
-
+	if(islower(c) > 0){
+		return(1);
+	} 
+	
 	return (0);
-
 }

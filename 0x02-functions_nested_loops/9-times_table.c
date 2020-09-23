8f4e4e11cc32d6293341cb5c7c9af159a2ccbbbb
ramsteven <ramsteven@gmail.com>
2020-09-23 18:54:07 +0000

    adding task 9 and holberton file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,35 +11,33 @@ void times_table(void)
 	int n;
 	int x;
 
-	for(x = 0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		for(n = 0; n <= 9; n++)
+		for (n = 0; n <= 9; n++)
 		{
 			if ((n * x) <= 9)
 			{
 				_putchar(' ');
-				_putchar((n * x)+'0');
+				_putchar((n * x) + '0');
 
-				if( n != 9 )
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+					if (n != 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
 			}
 			else if ((n * x) > 9)
-			{	
-				
-				_putchar(((n*x)/10)+'0');
-				_putchar(((n*x)%10)+'0'); 	
-			
-				if( n != 9)
-				{
-				  _putchar(',');
-				  _putchar(' ');
-				}
+			{
+				_putchar(((n * x) / 10) + '0');
+				_putchar(((n * x) % 10) + '0');
+
+					if  n != 9)
+					{
+					  _putchar(',');
+					  _putchar(' ');
+					}
 			}
 		}
 		_putchar('\n');
 	}
-
 }

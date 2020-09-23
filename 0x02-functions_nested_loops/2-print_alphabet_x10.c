d5207e3477f449adb44a9c4e8a1f99dfddc89640
ramsteven <ramsteven@gmail.com>
2020-09-23 01:00:12 +0000

    task2v3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-  * print_aphabet_x10- print alphabet 10 times
+  * print_alphabet_x10 - print alphabet 10 times
   * @void: nothing file specific
   *
   * Return: not return

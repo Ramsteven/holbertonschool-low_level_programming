c1633762b5c3c188b4c407545634e3c8daaba7a8
ramsteven <ramsteven@gmail.com>
2020-09-23 04:06:09 +0000

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,27 @@
 #include "holberton.h"
 
 /**
+  * _abs - check and print sign of a numb
+  * @n: numeber to get absolute value
+  *
+  * Return: 1 if n is zero, 0 if n is zero, -1 if n is n is less than zero
+  **/
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else if (n > 0)
+	{
+		return (n);
+	}
+		return (0);
+}
+
+
+/**
   *print_last_digit - print the las digit of a number
   *@n: number to get absolute value
   *
@@ -8,5 +29,9 @@
   */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int last;
+
+	last = _abs((n % 10));
+	_putchar(last + '0');
+	return (last);
 }

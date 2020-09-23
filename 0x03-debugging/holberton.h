8a9bf66112789ffaabdbc1eea3f398e50b86fedf
ramsteven <ramsteven@gmail.com>
2020-09-23 23:17:42 +0000

    adding task 3 and holberton file

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+/**Return number positive or negative **/
 void positive_or_negative(int i);
+
+/** Return the largest number**/
+int largest_number(int a, int b, int c);
 
 #endif /* HOLBERTON_H */

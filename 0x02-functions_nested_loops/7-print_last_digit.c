6d230131ba04eaa15231c294ac9b65e8e4cf58b1
ramsteven <ramsteven@gmail.com>
2020-09-23 02:36:45 +0000

    taks7 v1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+  *print_last_digit - print the las digit of a number
+  *@n: number to get absolute value
+  *
+  *Return: the value the las digit
+  */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}

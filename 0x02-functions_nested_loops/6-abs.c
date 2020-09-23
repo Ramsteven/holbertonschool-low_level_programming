f811d7404ac2b89c71394ffe1f0c0d8d87b0971e
ramsteven <ramsteven@gmail.com>
2020-09-23 02:28:41 +0000

    task 6 v3

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,20 @@
 #include "holberton.h"
-#include <stdlib.h>
 
 /**
-  *print_sign- check and print sign of a number
+  *_abs - check and print sign of a number
+  *@n: numeber to get absolute value
   *
   *Return: 1 if n is zero, 0 if n is zero, -1 if n is n is less than zero
   */
 int _abs(int n)
 {
-	return (abs(n));
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else if (n > 0)
+	{
+		return (n);
+	}
+	return (0);
 }

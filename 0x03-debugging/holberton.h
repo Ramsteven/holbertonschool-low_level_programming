fd5adbb70e6917cde7710ac238fed5feba214465
ramsteven <ramsteven@gmail.com>
2020-09-24 03:34:25 +0000

    adding files task 4 and holberton libraries

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -9,4 +9,8 @@ void positive_or_negative(int i);
 /** Return the largest number**/
 int largest_number(int a, int b, int c);
 
+/** Return dates years**/
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
 #endif /* HOLBERTON_H */

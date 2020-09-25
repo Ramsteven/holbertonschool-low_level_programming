0da390754d491f680436e38b6cad2f24ca4d3bc2
ramsteven <ramsteven@gmail.com>
2020-09-25 04:49:32 +0000

    adding task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,12 +8,11 @@
   **/
 void print_number(int n)
 {
-
 	if (n >= 0)
 	{
 		if (n / 10 != 0)
 			print_number(n / 10);
-		_putchar((n % 10) + '0');
+		_putchar(n % 10 + '0');
 	}
 	else
 	{
@@ -22,4 +21,3 @@ void print_number(int n)
 		print_number(n);
 	}
 }
-

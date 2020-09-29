760f6cb62eda5d6e591bb6d4d105b830d078942f
ramsteven <ramsteven@gmail.com>
2020-09-29 14:41:39 +0000

    Adding task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
+  * reset_to_98 - reset a varible with a pointer
+  * @*n: pointer argument for change
   *
-  *
-  *
-  *
+  * Return: not return
   **/
 void reset_to_98(int *n)
 {

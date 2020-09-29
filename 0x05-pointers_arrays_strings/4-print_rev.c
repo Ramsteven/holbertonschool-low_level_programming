597c25cf2e4a8ce04e3569340a5e183628b6e8f1
ramsteven <ramsteven@gmail.com>
2020-09-29 21:12:07 +0000

    adding task 4 and holberton file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 
 /**
-  * _print_rev - printe string in reverse
+  * print_rev - printe string in reverse
   * @s : given string
-  trlen(str) print value of a string in reverse
+  * print value of a string in reverse
   * Return: no return
   **/
 void print_rev(char *s)
 {
-         int i;
-	 int x;
+	int i;
+	int x;
 
 	while (*s != '\0')
 	{
-                 i++;
-                 s++;
- 	}
+		i++;
+		s++;
+	}
 
 	for (x = i ; x >= 0; x--)
 	{
 		s--;
 		_putchar(*s);
 	}
-	_putchar('\n');		
+	_putchar('\n');
 }

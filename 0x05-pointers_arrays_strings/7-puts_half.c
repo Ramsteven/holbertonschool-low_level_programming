90abe336a3728f402dc9a10cc6edf9b48d189b2e
ramsteven <ramsteven@gmail.com>
2020-10-01 05:14:51 +0000

    adding task 7 v 5

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,13 @@ void puts_half(char *str)
 	int i;
 	int size = _strlen(str);
 
-	if (size % 2 == 0)
+	if (size % 2 != 0)
 	{
-		for (i = (size - 1) / 2; i < size ; i++)
+		for (i = (size - 1) / 2 + 1; i < size ; i++)
 			_putchar(*(str + i));
 
 	}
-	else if (size % 2 != 0)
+	else if (size % 2 == 0)
 	{
 		for (i = size / 2; i < size ; i++)
 			_putchar(*(str + i));

69fd93d4389edf3920b360d341ac02edd1a72466
ramsteven <ramsteven@gmail.com>
2020-10-02 21:31:46 +0000

    adding task 6 v3

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,26 @@
 #include "holberton.h"
 
 /**
+  *cap_string - capitalizate the one string
+  *@n: gives string
   *
-  *
-  *
-  *
+  *Return: return the string with capitalization
   **/
 char *cap_string(char *n)
 {
-	int i= 0;
+	int i = 0;
 
-
-	for(; n[i] != '\0'; i++)
+	for (; n[i] != '\0'; i++)
 	{
-
-		if((n[i] == ' ' || n[i] ==  '\n' || n[i] == '\t'
-		|| n[i] == ',' || n[i] == ';' || n[i] == '.' 
-		|| n[i] == '!' || n[i] == '?' || n[i] == '"' 
-		|| n[i] == '(' || n[i] == ')' || n[i] == '{' 
-		|| n[i] == '}' ) && n[i+1] >= 'a' 
-		&& n[i+1] <= 'z'){
-		
-			n[i+1] = n[i+1]-32;
+		if ((n[i] == ' ' || n[i] ==  '\n' || n[i] == '\t'
+		|| n[i] == ',' || n[i] == ';' || n[i] == '.'
+		|| n[i] == '!' || n[i] == '?' || n[i] == '"'
+		|| n[i] == '(' || n[i] == ')' || n[i] == '{'
+		|| n[i] == '}') && n[i + 1] >= 'a'
+		&& n[i + 1] <= 'z')
+		{
+			n[i + 1] = n[i + 1] - 32;
 		}
-	}	
-	
-	return(n);	
+	}
+	return (n);
 }

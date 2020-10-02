4a2a755afeca92fae854d6cbf23777a2ed5bb1ff
ramsteven <ramsteven@gmail.com>
2020-10-02 21:16:58 +0000

    adding task 6 and holberton files

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -22,3 +22,5 @@ void reverse_array(int *a, int n);
 /** changes all lowercase letters of a string to uppercase*/
 char *string_toupper(char *);
 
+/** capitalizes all words of a string. */
+char *cap_string(char *);

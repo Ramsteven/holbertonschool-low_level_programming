91547a433d8ff928aaeab67c06844d6a475f0a50
ramsteven <ramsteven@gmail.com>
2020-10-05 18:14:28 +0000

    adding file task1 v1 an library file

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,2 +1,5 @@
 /** fill memomy with a constant byte*/
 char *_memset(char *s, char b, unsigned int n);
+
+/** function that copies memory area**/
+char *_memcpy(char *dest, char *src, unsigned int n);

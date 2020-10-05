f42ab874b969b6b56a323e5467641ac1e88b968e
ramsteven <ramsteven@gmail.com>
2020-10-05 18:02:41 +0000

    adding files task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+  *_memcpy- function that copies memory specific area.
+  *@dest: destination pointer
+  *@src: array that will copy
+  *@n: copy n times
+  *
+  *Return: pointer with the new values
+  **/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char temp;
+
+	for (i = 0; i <= n; i++)
+	{
+		temp  = src[i];
+		*dest = temp;
+		dest++;
+	}
+
+	return (dest);
+}

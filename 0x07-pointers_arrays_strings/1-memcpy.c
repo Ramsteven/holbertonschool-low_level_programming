4249eae64947e73d09d89500eff0dba2bcf7f774
ramsteven <ramsteven@gmail.com>
2020-10-05 18:27:11 +0000

    adding file task1 v1 an library file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char temp;
 
 	for (i = 0; i < n; i++)
 	{
-		temp  = src[i];
-		*dest = temp;
+		*dest = src[i];
 		dest++;
 	}
 

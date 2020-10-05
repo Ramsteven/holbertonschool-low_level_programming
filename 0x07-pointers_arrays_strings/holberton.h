114b220365b32ddd0fd67a5c668d301a002e1d61
ramsteven <ramsteven@gmail.com>
2020-10-05 23:29:32 +0000

    adding files task 3

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,3 +7,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 /** function that locates a character in a string**/
 char *_strchr(char *s, char c);
 
+/** function that legth of a prefix substring*/
+unsigned int _strspn(char *s, char *accept);
+

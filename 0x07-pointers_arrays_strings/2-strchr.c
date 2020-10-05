cf05e1c481c04b3ad9f50eaa7fd19e12f269946c
ramsteven <ramsteven@gmail.com>
2020-10-05 19:48:39 +0000

    adding files 2-strcr and holberton file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+  *_strchr- locates a character in a string
+  *@s : gives string
+  *@c : character to be locates
+  *
+  *Return: return no if no locates return pointer when locates
+  **/
+char *_strchr(char *s, char c)
+{
+	if ('\0' == s)
+		return ('\0');
+
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return ('\0');
+}

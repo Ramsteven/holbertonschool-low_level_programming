82f5b6ed80427fa7370e4809935d736c3144bed6
ramsteven <ramsteven@gmail.com>
2020-10-05 18:06:20 +0000

    adding files task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 	char temp;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= n && i != 0; i++)
 	{
 		temp  = src[i];
 		*dest = temp;

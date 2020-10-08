b0e4ff13828f4de1cc2ecf75116d6f375807dc3e
ramsteven <ramsteven@gmail.com>
2020-10-08 21:31:48 +0000

    adding task 7 v 4

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
   *is_palindrome- Write a function that returns the
@@ -13,10 +12,9 @@ int is_palindrome(char *s)
 	int i = 0;
 	int k = 0;
 
-	while (s[i] != '\0')
-		i++;
+	i = _strlen_recursion(s);
 
-	if (s == '\0')
+	if (*s == '\0')
 		return (1);
 
 	return (helper_palindrome(i - 1, k, s));
@@ -38,4 +36,18 @@ int helper_palindrome(int i, int k, char *s)
 		return (0);
 
 	return (helper_palindrome(i - 1, k + 1, s));
+}
+
+/**
+  *_strlen_recursion- function that print one string in reverse
+  *@s:gives string
+  *
+  *Return: return the length of a string
+  **/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }

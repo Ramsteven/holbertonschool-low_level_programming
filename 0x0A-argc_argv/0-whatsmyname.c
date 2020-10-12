84f0aafef6b75669f14c6521a0e94e4ee0a011b8
ramsteven <ramsteven@gmail.com>
2020-10-12 22:09:14 +0000

    adding folder 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+#include <stdio.h>
+/**
+  *main- function that get the value of the firts attribute
+  *@argc: the numbers of attributes received
+  *@argv: array with the attributes recivied
+  *prints: print the firts attribute.
+  *Return: 0 if all its ok
+  **/
+
+int main(int __attribute__ ((unused))  argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

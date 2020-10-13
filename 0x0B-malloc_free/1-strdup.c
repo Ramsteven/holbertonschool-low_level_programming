a2e5250a5bed2f79c95bb6861bc3870ca5c1ba0f
ramsteven <ramsteven@gmail.com>
2020-10-13 20:46:39 +0000

    adding task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#includei "holberton.h"
+#include <stdlib.h>
+
+/**
+  *_strdup-Write a function that returns a pointer to a newly allocated space.
+  *@str: string for save inside pointer
+  *
+  *Return: NULL if the *Str is null or p if not
+  **/
+char *_strdup(char *str)
+{
+	char *p;
+	int i = 0;
+	int k = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	p = malloc(sizeof(char) * i);
+
+	if (!p || !str)
+		return (NULL);
+
+	for (k = 0; k < i; k++)
+		p[k] = *(str + k);
+
+	return (p);
+}

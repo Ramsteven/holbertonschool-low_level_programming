5fa68ee27dc83308c9e14ffd1e4297fe47c2a3d0
ramsteven <ramsteven@gmail.com>
2020-10-13 19:22:07 +0000

    adding task 0 v1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,11 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i = 0;
 
-	if (size == 0)
-		return (NULL);
 
 	p = malloc(sizeof(char) * size);
+
+	if (size == 0 || !p)
+		return (NULL);
 
 	while (i < size)
 	{

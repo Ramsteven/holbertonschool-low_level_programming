97b410d1070200d9ebfa160b61309af4d081b4a2
ramsteven <ramsteven@gmail.com>
2020-10-13 04:44:28 +0000

    adding task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,6 +16,12 @@ int main(int argc, char *argv[])
 	int j;
 	int x = 0;
 
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)

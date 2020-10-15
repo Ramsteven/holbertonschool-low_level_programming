d7dd36fc7ecad6076c15af9d32abfc3db9dcda7f
ramsteven <ramsteven@gmail.com>
2020-10-15 04:43:32 +0000

    adding task 03

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *size-return size of a string.
+  *@str:gives string.
+  *
+  *Return: return size of a string.
+  **/
+int size(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+
+/**
+  *alloc_grid- concatenate two string.
+  *@width: with matrix
+  *@height: heigth matrix
+  *
+  *Return: NULL if had failures o negative number otherwise return NULL
+  **/
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	array = malloc(height * sizeof(int *));
+
+	if (array == NULL)
+	{
+		printf("out of memory\n");
+		return (NULL);
+	}
+
+	for (i = 0; i < width; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			printf("out of memory\n");
+			return (NULL);
+		}
+	}
+
+	return (array);
+}

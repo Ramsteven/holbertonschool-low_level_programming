a4fb8c12322a64cf9c203b5c951ebb2e9b5a0a01
ramsteven <ramsteven@gmail.com>
2020-10-19 20:04:24 +0000

    addingas task 4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+  *main-print a file executed
+  *@void: dont define argument
+  *
+  *Return: return 0.
+  **/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+

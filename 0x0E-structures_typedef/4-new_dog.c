f811c8532f8299205835a377f52dd5d2e994feb7
ramsteven <ramsteven@gmail.com>
2020-10-20 23:36:01 +0000

    adding task 06

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * _strlen - returns the length of a string
@@ -27,16 +25,12 @@ int _strlen(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = NULL;
-	char *nameDog;
-	char *ownerDog;
-	int i = 0;
-	int j = 0;
-	int len1;
-	int len2;
-	
+	char *nameDog, *ownerDog;
+	int i = 0, j = 0, len1, len2;
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-	
+
 	nameDog = malloc(sizeof(char) * len1 + 1);
 
 	ownerDog = malloc(sizeof(char) * len2 + 1);
@@ -45,32 +39,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (!d || !nameDog || !ownerDog)
 	{
-
-		free(nameDog);
-		free(ownerDog);
 		return (NULL);
-
 	}
 	else
 	{
-		while(i <= len1)
+		while (i < len1)
 		{
 			nameDog[i] = name[i];
 			i++;
 		}
-		
-		
-		
-		while(j <= len2)
+		nameDog[i] = 0;
+
+		while (j < len2)
 		{
-			ownerDog[i] = owner[j];
+			ownerDog[j] = owner[j];
 			j++;
 		}
-		
+
+		ownerDog[j] = 0;
 		(*d).name = nameDog;
 		(*d).age = age;
 		(*d).owner = ownerDog;
 	}
-
 	return (d);
 }

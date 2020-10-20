8f59b1d2cfb76ca0eb70d0549543faa8100c7563
ramsteven <ramsteven@gmail.com>
2020-10-20 20:25:57 +0000

    adding task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,24 +13,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = NULL;
-	char *n;
-	char *o;
+	char *nameDog;
+	char *ownerDog;
 
-	n = malloc(sizeof(name));
+	nameDog = malloc(sizeof(name));
 
-	o = malloc(sizeof(owner));
+	ownerDog = malloc(sizeof(owner));
 
 	d = malloc(sizeof(dog_t));
 
-	if (!d || !o || !n)
+	if (!d || !nameDog || !ownerDog)
 	{
+		free(nameDog);
+		free(ownerDog);
 		return (NULL);
+
 	}
 	else
 	{
-		(*d).name = name;
+		nameDog = name;
+		ownerDog = owner;
+		(*d).name = nameDog;
 		(*d).age = age;
-		(*d).owner = owner;
+		(*d).owner = ownerDog;
 	}
 
 	return (d);

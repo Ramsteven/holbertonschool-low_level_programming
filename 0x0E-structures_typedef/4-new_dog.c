69b619c33be067a943c2dd66102d37718cf36953
ramsteven <ramsteven@gmail.com>
2020-10-20 20:03:17 +0000

    adding task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *new_dog- function that create a new dog
+  *@name:adding task
+  *@age: age of dog
+  *@owner: owner dog
+  *
+  *Return: pointer of datatype dog_t
+  **/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = NULL;
+
+	d = malloc(sizeof(dog_t));
+
+	if (!d)
+	{
+		return (NULL);
+	}
+	else
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+
+	return (d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,10 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 /* function that prints a struct dog*/
 void print_dog(struct dog *d);
-
+/**alias fot struc dog*/
 typedef struct dog dog_t;
+
+/* function that creates a new dog*/
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif

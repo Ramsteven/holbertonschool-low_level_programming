b60dae68f65601db4392a98a16a7db73d9675f8c
ramsteven <ramsteven@gmail.com>
2020-10-20 01:16:17 +0000

    adding task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+  *struct dog - dog
+  *
+  *@name: name of the dog
+  *@age:age of the dog
+  *@owner: who own the dog
+  **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

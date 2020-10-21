69683b084bc0c2affcb4b904ea0f6f8b4f45eb8d
ramsteven <ramsteven@gmail.com>
2020-10-21 17:10:41 +0000

    adding task 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+  *print_name-print name with a function pointers
+  *@name: character received
+  *@f:function pointer received
+  *
+  *Return: no return
+  **/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
+
+/*function that prints a name */
+void print_name(char *name, void (*f)(char *));
+
+/*function that print a character*/
+int _putchar(char c);
+
+
+#endif

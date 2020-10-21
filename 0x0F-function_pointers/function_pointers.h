7409aa3f885e297da6f40f98e6882117dd3b87a7
ramsteven <ramsteven@gmail.com>
2020-10-21 19:39:43 +0000

    adding task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,15 @@
 #ifndef _FUNCTION_POINTERS_H
 #define _FUNCTION_POINTERS_H
 
+#include <stdlib.h>
+
 /*function that prints a name */
 void print_name(char *name, void (*f)(char *));
 
 /*function that print a character*/
 int _putchar(char c);
 
-
+/*function that executes a function given as a 
+ *parameter on each element *of an array.*/
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

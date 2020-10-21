23e98c3dcb51d5635c8934b95393a738199387c3
ramsteven <ramsteven@gmail.com>
2020-10-21 19:37:45 +0000

    adding task 01

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+  *array_iterator-unction that executes a function
+  *given as a parameter on each element of an array.
+  *@array: array received
+  *@size:  size received
+  *@action: action function received
+  *
+  *Return: nothing
+  **/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array && action && size > 0)
+
+	while (i <= size)
+	{
+		action(array[i]);
+		i++;
+	}
+}

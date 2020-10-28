bb082dd57767314ce0ec9b77d604bd6a4a5fe2b7
ramsteven <ramsteven@gmail.com>
2020-10-28 20:12:19 +0000

    adding task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,12 +8,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list Valist;
+	char *string;
 
 	va_start(Valist, n);
-
+	
 	for (; i < n; i++)
 	{
-		printf("%s", va_arg(Valist, char *));
+		string = va_arg(Valist, char *);
+		printf("%s", string == NULL ? "nill": string);
 
 		if (separator && i != n - 1 && separator[0] != 0)
 			printf("%c ", *separator);

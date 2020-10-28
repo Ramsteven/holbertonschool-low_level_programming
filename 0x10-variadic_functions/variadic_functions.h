0cd55749accf909be67c0ac78bb17221cff8d0de
ramsteven <ramsteven@gmail.com>
2020-10-28 18:59:08 +0000

    adding task 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,8 @@
 /*function that returns the sum of all its parameters*/
 int sum_them_all(const unsigned int n, ...);
 
+/*function that prints numbers, followed by a new line.*/
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 int _putchar(char n);
 

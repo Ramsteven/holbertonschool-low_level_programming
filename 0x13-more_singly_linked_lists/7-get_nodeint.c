83cf1ea3766fedda6d6c1d5c2aa27f88341bcdcc
ramsteven <ramsteven@gmail.com>
2020-11-04 18:21:28 +0000

    adding task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,6 +12,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *h;
 
+	if (!head || !(*head))
+		return (NULL);
+
 	h = head;
 
 	while (i++ < index)

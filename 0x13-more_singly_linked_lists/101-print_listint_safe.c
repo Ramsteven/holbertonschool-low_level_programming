b0bca480439c7b5d2fcd077948f13c8c858b3f8d
ramsteven <ramsteven@gmail.com>
2020-11-04 17:02:25 +0000

    adding exit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,7 +4,7 @@
   *@head: list received
   *
   *Return: numbers of nodes
-  **/ 
+  **/
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i = 0;
@@ -15,17 +15,17 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		result = head - head->next;
 		i++;
-		printf("[%p] %i\n",(void *)head, head->n);
+		printf("[%p] %i\n", (void *)head, head->n);
 
-		if(result > 0)
+		if (result > 0)
 			head = head->next;
 		else
 		{
-			printf("-> [%p] %i\n",(void *)head->next, head->next->n);
-			break;
+			printf("-> [%p] %i\n", (void *)head->next, head->next->n);
+			exit(98);
 		}
 
 
 	}
 	return (i);
-}	
+}

bf4b1cd828196b79f3a9ea32193519bb170a5f3b
ramsteven <ramsteven@gmail.com>
2020-11-05 01:45:37 +0000

    adding task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,12 +15,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listint_t *temp;
 
+
+	if (!head)
+		return (NULL);
+
 	current = *head;
 
 	temp = malloc(sizeof(listint_t));
 
+
 	if (!head || !temp)
 		return (NULL);
+
+	temp->n = n;    
+        temp->next = NULL;
 
 	if (!idx)
 	{
@@ -29,8 +37,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (current);
 	}
 
-	temp->n = n;
-	temp->next = NULL;
+	if(!(*head) && idx == 0)
+	{
+		*head = temp;
+		return (temp);
+	}
 
 	while (i++ < idx - 1)
 		current = current->next;

befd8688a274ef0ab0593906c0e4530ad3764db9
ramsteven <ramsteven@gmail.com>
2020-11-09 20:19:17 +0000

    adding edge case

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	/*create file*/
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-		return (free(buffer), 0);
+		return (-1);
 
 	/*write*/
 	if (text_content == NULL)

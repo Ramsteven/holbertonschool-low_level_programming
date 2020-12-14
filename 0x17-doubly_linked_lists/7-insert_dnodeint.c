5c73a59f8bc08b7fb9470a8ace3beb064ca3aec2
ramsteven <ramsteven@gmail.com>
2020-12-14 20:01:01 +0000

    adding file 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ *insert_dnodeint_at_index- function that return a node with nth
+ *@h: pointer that pointed a head of list
+ *@idx: index of list where the new node should be added.
+ *@n: number that the new added node should be hold.
+ *Return: return the new node
+ **/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h;
+	dlistint_t *new = NULL;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+
+	if (!tmp)
+		return (NULL);
+
+	for (; i < idx && tmp; i++)
+		tmp = tmp->next;
+
+
+	new->next = tmp;
+	new->prev = tmp->prev;
+	tmp->prev->next = new;
+	tmp->prev = new;
+
+	return (new);
+}

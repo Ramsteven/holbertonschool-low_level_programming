7c0e60a832e24030bfd655c37599d864f4a1600a
ramsteven <ramsteven@gmail.com>
2020-12-14 15:54:06 +0000

    adding firts commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_dlistint- function that print a n int hold inside of a dblinkedlist
+ *@h: head of list
+ *Return: amount of nodes of the list
+ **/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (!h->next && !h->prev)
+		return (0);
+
+	while (h)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}

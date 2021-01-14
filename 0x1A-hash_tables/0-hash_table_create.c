3982172ccb43adb70ea12081dae1ad41f6280cd6
ramsteven <ramsteven@gmail.com>
2021-01-14 17:06:19 +0000

    adding folder

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creat a hash table.
+ * @size: size of array
+ *
+ * Return: hash table created
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_table_t *table = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+
+	table->array = malloc(sizeof(hash_table_t) * size);
+	if (!table)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	table->size = size;
+
+	return (table);
+}

95df831b85fa85f480cc8a80a586587b4496b044
ramsteven <ramsteven@gmail.com>
2021-01-15 18:50:52 +0000

    adding hash_node

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-	hash_table_t *table = NULL;
+	hash_table_t *table;
 
 	if (size == 0)
 		return (NULL);
@@ -21,8 +21,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!table)
 		return (NULL);
 
-	table->array = malloc(sizeof(hash_table_t) * size);
-	if (!table->array)
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);

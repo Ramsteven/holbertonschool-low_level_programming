3173d2b27b508a71e15a7c9b6317418a4ce90d1f
ramsteven <ramsteven@gmail.com>
2021-01-16 20:12:13 +0000

    adding edge cases

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,6 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	hash_node_t *tmp;
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
 	if (!key || !ht || !(*key) || !value)
@@ -32,20 +31,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
 
-	tmp = ht->array[index];
-
-	if (ht->array[index] == NULL)
-		ht->array[index] = new_node;
-
-
-	if (tmp != NULL)
+	if (!(new_node->key) || !(new_node->value))
 	{
-
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
+		if (new_node->key)
+			free(new_node->key);
+		free(new_node);
+		return (0);
 	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+
+
 
 	return (1);
 }

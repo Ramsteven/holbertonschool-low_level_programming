a8598ca757fe3c9e8af50b2cabf8e22327d3e45e
ramsteven <ramsteven@gmail.com>
2021-01-16 20:29:03 +0000

    addig task

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,61 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "hash_tables.h"
-/**
- * hash_table_set - function that adds an element to the hash table.
- * @ht: the hash table you want to add or update
- * @key: is the key
- * @value:the value associated with the key.
- * Return: 1 if it succeeded, 0 if not
- */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int idx = 0;
-	hash_node_t *new = NULL;
-
-	if (!ht || !key || !(*key) || !value)
-		return (0);
-
-	idx = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[idx] && !(match(key, ht->array[idx], value)))
-		return (1);
-
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-		return (0);
-	new->key = strdup(key);
-	new->value = strdup(value);
-	if (!(new->key) || !(new->value))
-	{
-		if (new->key)
-			free(new->key);
-		free(new);
-		return (0);
-	}
-	new->next = ht->array[idx];
-	ht->array[idx] = new;
-	return (1);
-}
 
 /**
- *match - Check if a key value is already in a bucket.
- * @key: key
- * @arry: bucket
- * @value: node value to update
- * Return: 1 if not match and 0 if match
+ * hash_table_get - function that creat a hash table.
+ * @ht: hash table
+ * @key: key of element inside of hash_table
+ *
+ * Return: value with the key gives otherwise NULL
  */
-int match(const char *key, hash_node_t *arry, const char *value)
-{
-	hash_node_t *elment = arry;
 
-	while (elment)
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	unsigned long int index = 0;
+
+
+
+	if (!ht || ht->array == NULL || strlen(key) == 0 || !key || ht->size == 0)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp)
 	{
-		if (!(strcmp(elment->key, key)))
-		{
-			free(elment->value);
-			elment->value = strdup(value);
-			return (0);
-		}
-		elment = elment->next;
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
 	}
-	return (1);
+
+	return (NULL);
 }

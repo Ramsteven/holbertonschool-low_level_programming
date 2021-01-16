da3e121683bc678e01d4964d9142e62c76cb256c
ramsteven <ramsteven@gmail.com>
2021-01-16 19:22:55 +0000

    adding null to tmp

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,7 +15,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int i;
 	int count = 0;
-	hash_node_t *tmp;
+	hash_node_t *tmp = NULL;
 
 	if (!ht || ht->array == NULL)
 	{
@@ -31,7 +31,6 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (count > 0)
 				printf(", ");
-
 			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			count++;
 			tmp = tmp->next;
